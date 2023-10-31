8ecec6cc6be160730e5edf1a4aaca344cf953c52
Pratik Lamichhane <lamichhanepratik9@gmail.com>
2023-10-31 08:54:15 +0545

    added dashboard feature

--- puddle/puddle/settings.py
+++ puddle/puddle/settings.py
@@ -28,6 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = []
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL ='/'
+LOGOUT_REDIRECT_URL ='/'
+
 
 # Application definition
 
@@ -40,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'item',
+    'dashboard',
 ]
 
 MIDDLEWARE = [
