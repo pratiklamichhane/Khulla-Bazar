3c40e13cd24ff71430a4de4dd4242ecdec3c41cd
Pratik Lamichhane <lamichhanepratik9@gmail.com>
2023-10-31 16:22:18 +0545

    added search by category feature

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,18 +1,25 @@
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404, redirect
-from item.models import Item
+from item.models import Item , Category
 from .forms import NewItemForm , EditItemForm
 
 def items(request):
     query = request.GET.get('query', '')
+    category_id = request.GET.get('category', 0)
+    categories = Category.objects.all()
     items = Item.objects.filter(is_sold=False)
 
-    if query:
-        items = items.filter(name__icontains=query)
+    if category_id:
+        items = items.filter(category_id=category_id)
 
+    if query:
+        items = items.filter(Q(name__icontains=query)| Q(description__icontains=query))
     return render(request, 'item/items.html',{
         'items':items,
         'query':query,
+        'categories' : categories,
+        'category_id': int(category_id),
     })
 
 # Create your views here.
